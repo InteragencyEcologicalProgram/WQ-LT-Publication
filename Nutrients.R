df1c2e595134d5b1e61295650257b51958dbe65e
rosehartman <rosehartman@gmail.com>
2022-09-14 14:54:18 -0700

    Ran models for nutrients and chlorophyll

---
+++ Nutrients.R
@@ -0,0 +1,335 @@
+#Nutrient analyses
+
+
+library(tidyverse)
+library(lubridate)
+
+library(lme4)
+library(lmerTest)
+library(DHARMa)
+library(effects)
+library(DroughtData)
+
+
+#Replace my data with teh official version
+names(raw_chla_1975_2021)
+
+
+#Start with Ammonia
+#####################################################################
+#replace values below the reporting limit
+Ammonia <- raw_nutr_1975_2021 %>%
+  # Remove NA values in DissAmmonia
+  tidyr::drop_na(DissAmmonia) %>%
+  # Replace <RL values with random value
+  drt_replace_rl(DissAmmonia, DissAmmonia_Sign) %>%
+  # Calculate seasonal-regional averages
+  drt_avg_data(DissAmmonia, avg_type = "both", month_na = "relaxed") %>%
+  # Add year assignments
+  drt_add_yr_assign() %>%
+  mutate(YearType = factor(YearType, levels = c("Critical", "Dry", "Below Normal",
+                                                "Above Normal", "Wet")))
+
+
+logAmmonia <- raw_nutr_1975_2021 %>%
+  # Remove NA values in DissAmmonia
+  tidyr::drop_na(DissAmmonia) %>%
+  # Replace <RL values with random value
+  drt_replace_rl(DissAmmonia, DissAmmonia_Sign) %>%
+  #Log transformation
+  mutate(LogAm = log(DissAmmonia)) %>%
+  # Calculate seasonal-regional averages
+  drt_avg_data(LogAm, avg_type = "both", month_na = "relaxed") %>%
+  # Add year assignments
+  drt_add_yr_assign() %>%
+  mutate(YearType = factor(YearType, levels = c("Critical", "Dry", "Below Normal",
+                                                "Above Normal", "Wet")))
+
+#plot of Ammonia by water year type
+ggplot(logAmmonia, aes(x = YearType, y = LogAm, fill = YearType))+
+  geom_boxplot(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_yrtype()+
+  ylab("Ammonia mg/L log transformed")+
+  xlab("Year Type")+
+  theme_bw()+
+  scale_x_discrete(labels = c("C", "D", "B", "A", "W"))
+
+#plot of Ammonia by water year type
+ggplot(Ammonia, aes(x = YearType, y = DissAmmonia, fill = YearType))+
+  geom_boxplot(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_yrtype()+
+  ylab("Ammonia mg/L")+
+  xlab("Year Type")+
+  theme_bw()+
+  scale_x_discrete(labels = c("C", "D", "B", "A", "W"))
+
+#plot of Ammonia by Sac valley index
+ggplot(logAmmonia, aes(x = SVIndex, y = LogAm))+
+  geom_point(alpha = 0.7, aes(color =Drought))+
+  geom_smooth(method = "lm")+
+  facet_grid(Season~Region)+
+  drt_color_pal_drought(aes_type = "color")+
+  ylab("Ammonia mg/L (log-transformed)")+
+  xlab("Sac Valley Index")+
+  theme_bw()
+
+#plot ofammonia by drought
+ggplot(logAmmonia, aes(x = Drought, y = LogAm, fill = Drought))+
+  geom_boxplot(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_drought()+
+  ylab("Ammonia mg/L log transformed")+
+  xlab("Year Type")+
+  theme_bw()
+
+#Ammonia by year
+ggplot(Ammonia, aes(x = YearAdj, y = DissAmmonia, fill = YearType))+
+  geom_col(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_yrtype()+
+  ylab("Ammonia mg/L")+
+  theme_bw()
+
+####################################################################3
+#Ammonia models and box plots
+hist(Ammonia$DissAmmonia)
+hist(log(Ammonia$DissAmmonia))
+hist(logAmmonia$LogAm)
+
+#log-transformation is definitely better
+
+Am1 = lm(LogAm ~ Drought*Region*Season, data = logAmmonia)
+summary(Am1)
+plot(Am1)
+eff1 = allEffects(Am1, x.var = "Drought")
+plot(eff1)
+amp = emmeans(Am1, pairwise ~ Drought*Region*Season)
+plot(amp, comparisons = T)
+
+# I hate three-way interactions. Impossible to interpret
+
+
+Am2 = lm(LogAm ~Drought*Region + Drought*Season, data = logAmmonia)
+plot(Am2)
+eff2 = allEffects(Am2, x.var = "Drought")
+plot(eff2)
+summary(Am2)
+amp2 = emmeans(Am2, pairwise ~ Drought*Region)
+plot(amp2, comparisons = T)
+amp2b = emmeans(Am2, pairwise ~ Drought*Season)
+plot(amp2b, comparisons = T)
+
+#not very interesting
+
+#####################################################################
+#Nitrate and nitrite
+
+#replace values below the reporting limit
+Nitrate <- raw_nutr_1975_2021 %>%
+  # Remove NA values in DissNitrate
+  tidyr::drop_na(DissNitrateNitrite) %>%
+  # Replace <RL values with random value
+  drt_replace_rl(DissNitrateNitrite, DissNitrateNitrite_Sign) %>%
+  # Calculate seasonal-regional averages
+  drt_avg_data(DissNitrateNitrite, avg_type = "both", month_na = "relaxed") %>%
+  # Add year assignments
+  drt_add_yr_assign() %>%
+  mutate(YearType = factor(YearType, levels = c("Critical", "Dry", "Below Normal",
+                                                "Above Normal", "Wet")))
+
+
+logNitrate <- raw_nutr_1975_2021 %>%
+  # Remove NA values in DissNitrate
+  tidyr::drop_na(DissNitrateNitrite) %>%
+  # Replace <RL values with random value
+  drt_replace_rl(DissNitrateNitrite, DissNitrateNitrite_Sign) %>%
+  #Log transformation
+  mutate(LogNat = log(DissNitrateNitrite)) %>%
+  # Calculate seasonal-regional averages
+  drt_avg_data(LogNat, avg_type = "both", month_na = "relaxed") %>%
+  # Add year assignments
+  drt_add_yr_assign() %>%
+  mutate(YearType = factor(YearType, levels = c("Critical", "Dry", "Below Normal",
+                                                "Above Normal", "Wet")))
+
+#plot of Nitrate by water year type
+ggplot(logNitrate, aes(x = YearType, y = LogNat, fill = YearType))+
+  geom_boxplot(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_yrtype()+
+  ylab("Nitrate Nitrite (log-transformed)")+
+  xlab("Year Type")+
+  theme_bw()+
+  scale_x_discrete(labels = c("C", "D", "B", "A", "W"))
+
+ggplot(Nitrate, aes(x = YearType, y = DissNitrateNitrite, fill = YearType))+
+  geom_boxplot(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_yrtype()+
+  ylab("Nitrate mg/L")+
+  xlab("Year Type")+
+  theme_bw()+
+  scale_x_discrete(labels = c("C", "D", "B", "A", "W"))
+
+#plot of Nitrate by Sac valley index
+ggplot(logNitrate, aes(x = SVIndex, y = LogNat))+
+  geom_point(alpha = 0.7, aes(color =Drought))+
+  geom_smooth(method = "lm")+
+  facet_grid(Season~Region)+
+  drt_color_pal_drought(aes_type = "color")+
+  ylab("Nitrate nitrite mg/L (log-transformed)")+
+  xlab("Sac Valley Index")+
+  theme_bw()
+
+#plot ofNitrate by drought
+ggplot(Nitrate, aes(x = Drought, y = DissNitrateNitrite, fill = Drought))+
+  geom_boxplot(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_drought()+
+  ylab("Nitrate mg/L")+
+  xlab("Year Type")+
+  theme_bw()
+
+#Nitrate by year
+ggplot(Nitrate, aes(x = YearAdj, y = DissNitrateNitrite, fill = YearType))+
+  geom_col(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_yrtype()+
+  ylab("Nitrate mg/L")+
+  theme_bw()
+
+####################################################################3
+#Nitrate models and box plots
+hist(Nitrate$DissNitrateNitrite)
+hist(log(Nitrate$DissNitrateNitrite))
+hist(logNitrate$LogNat)
+
+#log-transformation is definitely better
+
+Nat1 = lm(LogNat ~ Drought*Region*Season, data = logNitrate)
+summary(Nat1)
+plot(Am1)
+eff1 = allEffects(Chl1, x.var = "Drought")
+plot(eff1)
+# I hate three-way interactions. Impossible to interpret
+
+
+Nat2 = lm(LogNat ~Drought*Region + Drought*Season, data = logNitrate)
+plot(Nat2)
+eff2 = allEffects(Nat2, x.var = "Drought")
+plot(eff2)
+summary(Nat2)
+pairsNN =  emmeans(Nat2, pairwise ~ Drought*Region)
+plot(pairsNN, comparisons = T)
+pairsNN2 =  emmeans(Nat2, pairwise ~ Drought*Season)
+plot(pairsNN2, comparisons = T)
+
+#OK, higher nitriate during drought years in the Confluence and Suisun.
+
+
+
+#####################################################################
+#Phosphorus
+
+#replace values below the reporting limit
+Phos <- raw_nutr_1975_2021 %>%
+  # Remove NA values in DissNitrate
+  tidyr::drop_na(DissOrthophos) %>%
+  # Replace <RL values with random value
+  drt_replace_rl(DissOrthophos, DissOrthophos_Sign) %>%
+  # Calculate seasonal-regional averages
+  drt_avg_data(DissOrthophos, avg_type = "both", month_na = "relaxed") %>%
+  # Add year assignments
+  drt_add_yr_assign() %>%
+  mutate(YearType = factor(YearType, levels = c("Critical", "Dry", "Below Normal",
+                                                "Above Normal", "Wet")))
+
+
+logPhos <- raw_nutr_1975_2021 %>%
+  # Remove NA values in DissNitrate
+  tidyr::drop_na(DissOrthophos) %>%
+  # Replace <RL values with random value
+  drt_replace_rl(DissOrthophos, DissOrthophos_Sign) %>%
+  # Calculate seasonal-regional averages
+  mutate(LogPhos = log(DissOrthophos)) %>%
+  # Calculate seasonal-regional averages
+  drt_avg_data(LogPhos, avg_type = "both", month_na = "relaxed") %>%
+  # Add year assignments
+  drt_add_yr_assign() %>%
+  mutate(YearType = factor(YearType, levels = c("Critical", "Dry", "Below Normal",
+                                                "Above Normal", "Wet")))
+
+#plot of Phosphorus by water year type
+ggplot(logPhos, aes(x = YearType, y = LogPhos, fill = YearType))+
+  geom_boxplot(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_yrtype()+
+  ylab("Orthophosphate mg/L (log-transformed)")+
+  xlab("Year Type")+
+  theme_bw()+
+  scale_x_discrete(labels = c("C", "D", "B", "A", "W"))
+
+ggplot(Phos, aes(x = YearType, y = DissOrthophos, fill = YearType))+
+  geom_boxplot(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_yrtype()+
+  ylab("Orthophosphate mg/L")+
+  xlab("Year Type")+
+  theme_bw()+
+  scale_x_discrete(labels = c("C", "D", "B", "A", "W"))
+
+#plot of Nitrate by Sac valley index
+ggplot(logPhos, aes(x = SVIndex, y = LogPhos))+
+  geom_point(alpha = 0.7, aes(color =Drought))+
+  geom_smooth(method = "lm")+
+  facet_grid(Season~Region)+
+  drt_color_pal_drought(aes_type = "color")+
+  ylab("Orthophosphate mg/L (log-transformed)")+
+  xlab("Sac Valley Index")+
+  theme_bw()
+
+#Wow. Phosphorus actuallylooks pretty good there.
+
+ggplot(Phos, aes(x = Drought, y = DissOrthophos, fill = Drought))+
+  geom_boxplot(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_drought()+
+  ylab("Phosphorus mg/L")+
+  xlab("Year Type")+
+  theme_bw()
+
+#Nitrate by year
+ggplot(Phos, aes(x = YearAdj, y = DissOrthophos, fill = YearType))+
+  geom_col(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_yrtype()+
+  ylab("Nitrate mg/L")+
+  theme_bw()
+
+####################################################################3
+#Nitrate models and box plots
+hist(Phos$DissOrthophos)
+hist(log(Phos$DissOrthophos))
+hist(logPhos$LogPhos)
+
+#Meh. Not sure there
+Phosx = lm(DissOrthophos ~ Drought*Region*Season, data = Phos)
+Phos1 = lm(LogPhos ~ Drought*Region*Season, data = logPhos)
+summary(Phos1)
+plot(Phos1)
+eff1 = allEffects(Chl1, x.var = "Drought")
+plot(eff1)
+# I hate three-way interactions. Impossible to interpret
+
+
+Phos2 = lm(LogPhos ~Drought*Region + Drought*Season, data = logPhos)
+plot(Phos2)
+eff2 = allEffects(Phos2, x.var = "Drought")
+plot(eff2)
+summary(Phos2)
+pairsP =  emmeans(Phos2, pairwise ~ Drought*Region)
+plot(pairsP, comparisons = T)
+plot(emmeans(Phos2, pairwise ~ Drought), comparisons = T)
+#major effect of drought on phosphorus! NOthing else, but yest phosphorus!
--- chlorophyll.R
+++ chlorophyll.R
@@ -1,67 +1,78 @@
 # Chlorophyll
 
+#devtools::install_github("mountaindboz/DroughtData", ref = "v2.0.0", build = FALSE)
+
 library(tidyverse)
 library(lubridate)
+
 library(lme4)
 library(lmerTest)
 library(DHARMa)
 library(effects)
 library(DroughtData)
+library(emmeans)
+# #test
+#
+# stas = c("P8", "D8", "MD10", "MD10A", "D7", "D6", "D4", "D28A", "D26", "D22", "D19", "D16", "D12", "D10", "C3", "C3A")
+#
+# #Chlorophyll data from KEith
+# Chl = read_csv("data/chla_data_stats_LT2.csv")
+# yrs = read_csv("data/yearassignments.csv")
+#
+# #Add NCRO
+# NCRO = read.csv("data/hab_nutr_chla_mvi.csv") %>%
+#   filter(Source %in% c("DWR_NCRO", "NCRO")) %>%
+#   rename(chlaAvg = Chlorophyll) %>%
+#   mutate(chlaAvg_log10 = log(chlaAvg, 10),
+#          month = month(Date),
+#          ds_year = case_when(month %in% c(11,12) ~ year(Date) + 1,
+#                              TRUE ~ year(Date)))
 
-#test
+#Replace my data with teh official version
+names(raw_chla_1975_2021)
 
-stas = c("P8", "D8", "MD10", "MD10A", "D7", "D6", "D4", "D28A", "D26", "D22", "D19", "D16", "D12", "D10", "C3", "C3A")
-
-#Chlorophyll data from KEith
-Chl = read_csv("data/chla_data_stats_LT2.csv")
-yrs = read_csv("data/yearassignments.csv")
-
-#Add NCRO
-NCRO = read.csv("data/hab_nutr_chla_mvi.csv") %>%
-  filter(Source %in% c("DWR_NCRO", "NCRO")) %>%
-  rename(chlaAvg = Chlorophyll) %>%
-  mutate(chlaAvg_log10 = log(chlaAvg, 10),
-         month = month(Date),
-         ds_year = case_when(month %in% c(11,12) ~ year(Date) + 1,
-                             TRUE ~ year(Date)))
-
-
+#replace values below the reporting limit
+Chla <- raw_chla_1975_2021 %>%
+  # Remove NA values in DissAmmonia
+  tidyr::drop_na(Chlorophyll) %>%
+  # Replace <RL values with random value
+  drt_replace_rl(Chlorophyll, Chlorophyll_Sign) %>%
+  #log transform chlorophyll
+  mutate(LogChl = log(Chlorophyll)) %>%
+  # Calculate seasonal-regional averages
+  drt_avg_data(LogChl, avg_type = "both", month_na = "relaxed") %>%
+  # Add year assignments
+  drt_add_yr_assign() %>%
+  mutate(YearType = factor(YearType, levels = c("Critical", "Dry", "Below Normal",
+                                                "Above Normal", "Wet")))
 
 
-#ok, beautiful! do some averaging
-Chl2 = Chl %>%
-  filter(Station %in% stas) %>% 
-  group_by(Region, Season, month, ds_year) %>%
-  summarize(Chla = mean(chlaAvg)) %>%
-  group_by( Region, Season, ds_year) %>%
-  summarize(Chla = mean(Chla)) %>%
-  group_by(Season, ds_year) %>%
-  summarize(Chla = mean(Chla), logChl = log(Chla)) %>%
-  rename(YearAdj = ds_year)
+#replace values below the reporting limit
+ChlaA <- raw_chla_1975_2021 %>%
+  # Remove NA values in DissAmmonia
+  tidyr::drop_na(Chlorophyll) %>%
+  # Replace <RL values with random value
+  drt_replace_rl(Chlorophyll, Chlorophyll_Sign) %>%
+   # Calculate seasonal-regional averages
+  drt_avg_data(Chlorophyll, avg_type = "both", month_na = "relaxed") %>%
+  # Add year assignments
+  drt_add_yr_assign() %>%
+  mutate(YearType = factor(YearType, levels = c("Critical", "Dry", "Below Normal",
+                                                "Above Normal", "Wet")))
 
-Chl2reg = Chl %>%
-  filter(Station %in% stas) %>% 
-  group_by(Region, Season, month, ds_year) %>%
-  summarize(Chla = mean(chlaAvg)) %>%
-  group_by( Region, Season, ds_year) %>%
-  summarize(Chla = mean(Chla), logChl = log(Chla)) %>%
-  rename(Year = ds_year) %>%
-  left_join(yrs) %>%
-  mutate(Yr_type = factor(Yr_type, levels = c("Critical", "Dry", "Below Normal", "Above Normal", "Wet")))# %>%
-  #filter(Year > 1995)
 
 #plot of chlorophyll by water year type
-ggplot(Chl2reg, aes(x = Yr_type, y = logChl, fill = Yr_type))+
+ggplot(Chla, aes(x = YearType, y = LogChl, fill = YearType))+
   geom_boxplot(alpha = 0.7)+
   facet_grid(Season~Region)+
   drt_color_pal_yrtype()+
   ylab("Chlorophyl ug/L (log-transformed)")+
   xlab("Year Type")+
   theme_bw()+
-  scale_x_discrete(labels = c("C", "D", "B", "A", "W")) 
+  scale_x_discrete(labels = c("C", "D", "B", "A", "W"))
 
-#plot of chlorophyll by water year type
-ggplot(Chl2reg, aes(x = Index, y = logChl))+
+#plot of chlorophyll by Sac valley index
+ggplot(Chla, aes(x = SVIndex, y = LogChl))+
   geom_point(alpha = 0.7, aes(color =Drought))+
   geom_smooth(method = "lm")+
   facet_grid(Season~Region)+
@@ -71,7 +82,7 @@ ggplot(Chl2reg, aes(x = Index, y = logChl))+
   theme_bw()
 
 #plot of chlorophyll by drought
-ggplot(Chl2reg, aes(x = Drought, y = logChl, fill = Drought))+
+ggplot(Chla, aes(x = Drought, y = LogChl, fill = Drought))+
   geom_boxplot(alpha = 0.7)+
   facet_grid(Season~Region)+
  drt_color_pal_drought()+
@@ -79,8 +90,16 @@ ggplot(Chl2reg, aes(x = Drought, y = logChl, fill = Drought))+
   xlab("Year Type")+
   theme_bw()
 
+ggplot(ChlaA, aes(x = Drought, y = Chlorophyll, fill = Drought))+
+  geom_boxplot(alpha = 0.7)+
+  facet_grid(Season~Region)+
+  drt_color_pal_drought()+
+  ylab("Chlorophyl ug/L")+
+  xlab("Year Type")+
+  theme_bw()
+
 #plot of chlorophyll by water year type - just hte more recent years
-ggplot(filter(Chl2reg, Year > 1989), aes(x = Yr_type, y = logChl, fill = Yr_type))+
+ggplot(filter(Chla, YearAdj > 1989), aes(x = YearType, y = LogChl, fill = YearType))+
   geom_boxplot(alpha = 0.7)+
   facet_grid(Season~Region)+
   drt_color_pal_yrtype()+
@@ -91,112 +110,107 @@ ggplot(filter(Chl2reg, Year > 1989), aes(x = Yr_type, y = logChl, fill = Yr_type
 #Bleh, nothing going on there
 
 #plot of chlorophyll by year
-ggplot(Chl2reg, aes(x = Year, y = logChl, fill = Yr_type))+
+ggplot(Chla, aes(x = YearAdj, y = LogChl, fill = YearType))+
   geom_col(alpha = 0.7)+
   facet_grid(Season~Region)+
   drt_color_pal_yrtype()+
   ylab("Chlorophyl ug/L (log-transformed)")+
   theme_bw()
 
-ggplot(Chl2reg, aes(x = Year, y = logChl, fill = Drought))+
+ggplot(Chla, aes(x = YearAdj, y = LogChl, fill = Drought))+
   geom_col(alpha = 0.7)+
   facet_grid(Season~Region)+
   drt_color_pal_drought()+
   ylab("Chlorophyl ug/L (log-transformed)")+
   theme_bw()
 
-
-#plot of raw chlrophyll data by day of year
-Chl = mutate(Chl, Yday = yday(Date), Yr_type = factor(ds_year_type, 
-                                                      levels = c("Critical", "Dry", "Below Normal", "Above Normal", "Wet"),
-                                                      ordered = TRUE)) %>%
-  filter(Station %in% stas)
-
-ggplot(Chl, aes(x = Yday, y = chlaAvg_log10, color = Yr_type))+
-  geom_point(alpha = 0.3)+
-  geom_smooth()+
-  drt_color_pal_yrtype(aes_type = "color")+
-  facet_wrap(~Region)+
-  theme_bw()
+#
+# #plot of raw chlrophyll data by day of year
+# Chl = mutate(raw_chla_1975_2021, Yday = yday(Date))
+#
+# ggplot(Chl, aes(x = Yday, y = chlaAvg_log10, color = Yr_type))+
+#   geom_point(alpha = 0.3)+
+#   geom_smooth()+
+#   drt_color_pal_yrtype(aes_type = "color")+
+#   facet_wrap(~Region)+
+#   theme_bw()
 
 ###############################################################################
 #Let's bin the data by >10 versus < 10
-Chl = mutate(Chl, bloom = case_when(chlaAvg > 10 ~ TRUE,
-                                    TRUE ~ FALSE)) %>%
-  filter(Region != "North Delta")
-
-
-#Binomial model of blooms
-#we don't have enough long-term data from the North Delta to include it.
-
-cmod = glmer(bloom ~ Region + Season+Yr_type + (1|Station) + (1|ds_year), family = "binomial", data = Chl)
-summary(cmod)
-library(visreg)
-visreg(cmod)
-
-cmod2 = glmer(bloom ~ Region + Season+Yr_type + (1|Station) + (1|ds_year), family = "binomial", data = Chl)
-summary(cmod2)
-plot(cmod2)
-visreg(cmod2)
-
-
-
-#Try it another way, I"m not sure how to tdo this right
-#get rid of Winter, because it's not interesting, and filter it to post-clam crash.
-
-Chlsum = Chl %>%
-  rename(Year = ds_year) %>%
-  left_join(yrs) %>%
-  group_by(Region, Season, Drought, Yr_type, month, Year) %>%
-  summarize(Presence = length(bloom[which(bloom)]), Absence = length(bloom[which(!bloom)]), N = n()) #%>%
-  #filter(Season != "Winter",ds_year >1989)
-
-cmod3 = glmer(cbind(Presence, Absence) ~ Region*Yr_type+ (1|Year), family = "binomial", data = Chlsum)
-summary(cmod3)
-plot(cmod3)
-visreg(cmod3)
-visreg(cmod3, xvar = "Yr_type", by = "Region")
-visreg(cmod3, xvar = "Region", by = "Yr_type")
-
-res = simulateResiduals(cmod3)
-plot(res)
-
-#let's visualize
-
-Chlsumlong = pivot_longer(Chlsum, cols = c(Presence, Absence), names_to = "bloom", values_to = "frequency")
-ggplot(Chlsumlong, aes(x = Year, y = frequency, fill = bloom)) + 
-  geom_col()+ facet_grid(Season~Region)
-
-Chlsum = mutate(Chlsum, percent = Presence/(Absence + Presence),
-                Yr_type = factor(Yr_type, 
-                                 levels = c("Critical", "Dry", "Below Normal", "Above Normal", "Wet"),
-                                 ordered = TRUE))
-
-ggplot(Chlsum, aes(x = Year, y = percent, fill = Yr_type)) + geom_col()+
-  facet_grid(Season~Region) + drt_color_pal_yrtype()+
-  ylab("Percent of CHLa samples over 10ug/L")
-
-ggplot(Chlsum, aes(x = Year, y = percent, fill = Drought)) + geom_col()+
-  facet_grid(Season~Region) + drt_color_pal_drought()+
-  ylab("Percent of CHLa samples over 10ug/L")
-
-ggplot(Chlsum, aes(x = Drought, y = percent, fill = Drought)) + geom_boxplot()+
-  facet_grid(Season~Region) + drt_color_pal_drought()+
-  ylab("Percent of CHLa samples over 10ug/L")
-
-ggplot(Chlsum, aes(x = Yr_type, y = percent, fill = Yr_type)) + 
-  geom_boxplot(alpha = 0.5)+
-  facet_grid(Season~Region) + drt_color_pal_yrtype()+
-  ylab("Percent of CHLa samples over 10ug/L")+
-  theme_bw()
-
+# Chl = mutate(Chl, bloom = case_when(chlaAvg > 10 ~ TRUE,
+#                                     TRUE ~ FALSE)) %>%
+#   filter(Region != "North Delta")
+#
+#
+# #Binomial model of blooms
+# #we don't have enough long-term data from the North Delta to include it.
+#
+# cmod = glmer(bloom ~ Region + Season+Yr_type + (1|Station) + (1|ds_year), family = "binomial", data = Chl)
+# summary(cmod)
+# library(visreg)
+# visreg(cmod)
+#
+# cmod2 = glmer(bloom ~ Region + Season+Yr_type + (1|Station) + (1|ds_year), family = "binomial", data = Chl)
+# summary(cmod2)
+# plot(cmod2)
+# visreg(cmod2)
+#
+#
+#
+# #Try it another way, I"m not sure how to tdo this right
+# #get rid of Winter, because it's not interesting, and filter it to post-clam crash.
+#
+# Chlsum = Chl %>%
+#   rename(Year = ds_year) %>%
+#   left_join(yrs) %>%
+#   group_by(Region, Season, Drought, Yr_type, month, Year) %>%
+#   summarize(Presence = length(bloom[which(bloom)]), Absence = length(bloom[which(!bloom)]), N = n()) #%>%
+#   #filter(Season != "Winter",ds_year >1989)
+#
+# cmod3 = glmer(cbind(Presence, Absence) ~ Region*Yr_type+ (1|Year), family = "binomial", data = Chlsum)
+# summary(cmod3)
+# plot(cmod3)
+# visreg(cmod3)
+# visreg(cmod3, xvar = "Yr_type", by = "Region")
+# visreg(cmod3, xvar = "Region", by = "Yr_type")
+#
+# res = simulateResiduals(cmod3)
+# plot(res)
+#
+# #let's visualize
+#
+# Chlsumlong = pivot_longer(Chlsum, cols = c(Presence, Absence), names_to = "bloom", values_to = "frequency")
+# ggplot(Chlsumlong, aes(x = Year, y = frequency, fill = bloom)) +
+#   geom_col()+ facet_grid(Season~Region)
+#
+# Chlsum = mutate(Chlsum, percent = Presence/(Absence + Presence),
+#                 Yr_type = factor(Yr_type,
+#                                  levels = c("Critical", "Dry", "Below Normal", "Above Normal", "Wet"),
+#                                  ordered = TRUE))
+#
+# ggplot(Chlsum, aes(x = Year, y = percent, fill = Yr_type)) + geom_col()+
+#   facet_grid(Season~Region) + drt_color_pal_yrtype()+
+#   ylab("Percent of CHLa samples over 10ug/L")
+#
+# ggplot(Chlsum, aes(x = Year, y = percent, fill = Drought)) + geom_col()+
+#   facet_grid(Season~Region) + drt_color_pal_drought()+
+#   ylab("Percent of CHLa samples over 10ug/L")
+#
+# ggplot(Chlsum, aes(x = Drought, y = percent, fill = Drought)) + geom_boxplot()+
+#   facet_grid(Season~Region) + drt_color_pal_drought()+
+#   ylab("Percent of CHLa samples over 10ug/L")
+#
+# ggplot(Chlsum, aes(x = Yr_type, y = percent, fill = Yr_type)) +
+#   geom_boxplot(alpha = 0.5)+
+#   facet_grid(Season~Region) + drt_color_pal_yrtype()+
+#   ylab("Percent of CHLa samples over 10ug/L")+
+#   theme_bw()
+#
 ############################################################
 #Pre-2000 versus post-2000
 
-Chl2b = mutate(Chl2, Regime = case_when(YearAdj < 2000 ~ "long-term",
-                                     YearAdj >= 2000 ~ "Short-Term")) %>%
-  rename(Year = YearAdj) %>%
-  left_join((yrs))
+Chl2b = mutate(Chla, Regime = case_when(YearAdj < 2000 ~ "long-term",
+                                     YearAdj >= 2000 ~ "Short-Term"))
 
 Chlb = mutate(Chl, Regime = case_when(ds_year < 2000 ~ "long-term",
                                       ds_year >= 2000 ~ "Short-Term"), Date = date(Date)) %>%
@@ -205,12 +219,12 @@ Chlb = mutate(Chl, Regime = case_when(ds_year < 2000 ~ "long-term",
   left_join(DF)
 
 Chlbreg = mutate(Chl2reg, Regime = case_when(Year < 2000 ~ "long-term",
-                                      Year >= 2000 ~ "Short-Term")) 
+                                      Year >= 2000 ~ "Short-Term"))
 
 ggplot(Chlb, aes(x = log(OUT), y = chlaAvg_log10, color = Regime)) + geom_point()+
   geom_smooth(method = "lm")
 
-ggplot(Chl2b, aes(x = Index, y = logChl, color = Regime)) + geom_point()+
+ggplot(Chl2b, aes(x = SVIndex, y = LogChl, color = Regime)) + geom_point()+
   geom_smooth(method = "lm")+
   facet_grid(~Season)+ ylab("Seasonal average chlorophyll")+
   xlab("Sac Valley Index (annual)")
@@ -220,3 +234,45 @@ ggplot(Chlbreg, aes(x = Index, y = logChl, color = Regime)) + geom_point()+
   geom_smooth(method = "lm")+
   facet_grid(Region~Season)+ ylab("Seasonal average chlorophyll")+
   xlab("Sac Valley Index (annual)")
+
+####################################################################3
+#final models and box plots
+library(broom)
+Chl1 = lm(LogChl ~ Drought*Region*Season, data = Chla)
+summary(Chl1)
+plot(Chl1)
+eff1 = allEffects(Chl1, x.var = "Drought")
+plot(eff1)
+Chl1res = tidy(Chl1) %>%
+  mutate(Stars = case_when(p.value < 0.05 & p.value > 0.01 ~ "*",
+                           p.value < 0.01 & p.value > 0.001 ~ "**",
+                           p.value < 0.001 ~ "***"))
+# I hate three-way interactions. Impossible to interpret
+
+
+Chl2 = lm(LogChl ~Drought*Region + Drought*Season, data = Chla)
+plot(Chl2)
+eff2 = allEffects(Chl2, x.var = "Drought")
+plot(eff2)
+summary(Chl2)
+pairsCHL = emmeans(Chl2, pairwise ~ Drought*Region)
+plot(pairsCHL, comparison = T)
+#Nada. Nothing going on.
+pairsCHL2 = emmeans(Chl2, pairwise ~ Drought*Season)
+plot(pairsCHL2, comparison = T)
+
+Chl3 = lm(LogChl ~Region*Drought + Season, data = Chla)
+plot(Chl3)
+eff3 = allEffects(Chl3)
+plot(eff3, x.var = "Drought")
+summary(Chl3)
+pairsCHL = emmeans(Chl2, pairwise ~ Drought*Region)
+plot(pairsCHL, comparison = T)
+#Nada. Nothing going on.
+pairsCHL2 = emmeans(Chl2, pairwise ~ Drought*Season)
+plot(pairsCHL2, comparison = T)
+
+library(MuMIn)
+AICc(Chl2)
+AICc(Chl1)
+AICc(Chl3)

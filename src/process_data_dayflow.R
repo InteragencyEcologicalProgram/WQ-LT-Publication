279bb11271bfa02df78dbbee06e4dde2203574ba
Dave Bosworth <David.Bosworth@water.ca.gov>
2023-06-29 16:28:01 -0700

    Adding cumulative sums and totals and period year classifications to the processed dayflow data

--- src/process_data_dayflow.R
+++ src/process_data_dayflow.R
@@ -9,7 +9,7 @@ library(here)
 library(conflicted)
 
 # Declare package conflict preferences
-conflicts_prefer(dplyr::filter())
+conflicts_prefer(dplyr::filter(), dplyr::lag())
 
 # Source global data processing functions
 source(here("src/global_data_proc_func.R"))
@@ -32,24 +32,58 @@ df_dayflow <- ls_dayflow %>%
   # Convert date column to date and Inflow and Outflow to numeric
   mutate(
     Date = date(parse_date_time(Date, c("mdY", "Ymd"))),
-    across(contains("flow"), as.numeric)
-  )
-
-# Finish cleaning Dayflow data
-dayflow <- df_dayflow %>%
-  # Add Water Year and water day of year columns
-  mutate(
+    across(contains("flow"), as.numeric),
+    # Add Water Year and water day of year columns
     WaterYear = calc_wy(Date),
     WYday = calc_wy_day(Date)
   ) %>%
   # Filter to WY 1975-2021
-  filter(WaterYear %in% 1975:2021) %>%
+  filter(WaterYear %in% 1975:2021)
+
+# Calculate cumulative sums and totals for each water year
+df_dayflow_sum <- df_dayflow %>%
+  group_by(WaterYear)%>%
+  arrange(WYday)%>%
+  mutate(across(c(InflowTotal, Outflow), list(cum = ~ cumsum(.x), sum = ~ sum(.x)))) %>%
+  ungroup()
+
+# Import year assignments
+df_yr_type <- read_csv(here("data/raw/year_assignments.csv"))
+
+# Calculate period years for each drought classification
+df_drt_period_yrs <- df_yr_type %>%
+  select(Year, Drought) %>%
+  arrange(Drought, Year) %>%
+  group_by(Drought) %>%
+  mutate(Lag = Year - lag(Year, order_by = Year)) %>%
+  ungroup() %>%
+  mutate(
+    Start = if_else(is.na(Lag) | Lag > 1, TRUE, FALSE),
+    Period_ID = 1:n(),
+    Period_ID = if_else(Start, Period_ID, NA_integer_),
+    Period_ID = as.integer(as.factor(Period_ID))
+  ) %>%
+  fill(Period_ID, .direction = "down") %>%
+  group_by(Period_ID) %>%
+  arrange(Year) %>%
+  mutate(
+    Period_year = 1:n(),
+    Period_year_cat = if_else(Period_year >= 3, "3+", as.character(Period_year))
+  ) %>%
+  ungroup() %>%
+  select(Year, Drought, starts_with("Period_year"))
+
+# Add period years and drought classifications to summed Dayflow data and finish cleaning
+dayflow <- df_dayflow_sum %>%
+  left_join(df_drt_period_yrs, by = join_by(WaterYear == Year)) %>%
   select(
     Date,
     WaterYear,
     WYday,
-    InflowTotal,
-    Outflow
+    Drought,
+    starts_with("Period"),
+    starts_with("InflowTotal"),
+    starts_with("Outflow")
   ) %>%
   arrange(Date)
 

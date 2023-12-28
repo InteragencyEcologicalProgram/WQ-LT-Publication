ef4451483ef45eedbad4e10bdc37a70214b37a72
Dave Bosworth <David.Bosworth@water.ca.gov>
2023-12-28 10:38:53 -0800

    Improving survey summary table to be formatted closer to what was used in the publication

--- src/analysis/survey_por_summ_tbl.R
+++ src/analysis/survey_por_summ_tbl.R
@@ -10,40 +10,39 @@ library(qs)
 library(fs)
 library(here)
 
+# Source global data processing functions
+source(here("src/data_processing/global_data_proc_func.R"))
+
 # Import discrete measurement data for all WQ, nutrient, and chlorophyll parameters
 fp_dwq <- dir_ls(here("data/interim"), regexp = "raw.+\\.qs$")
 df_dwq <- map(fp_dwq, qread) %>% list_rbind()
 
-# Define factor level for Parameter
-param_lev <- c(
-  "Temperature",
-  "Salinity",
-  "Secchi",
-  "DissAmmonia",
-  "DissNitrateNitrite",
-  "DissOrthophos",
-  "Chlorophyll"
-)
-
 # Summarize periods of record for each survey-parameter combination
 df_survey_summ <- df_dwq %>%
-  mutate(
-    # Change a few survey names
-    Source = case_when(
-      Source == "20mm" ~ "20 mm",
-      Source == "Baystudy" ~ "Bay Study",
-      Source == "USGS_CAWSC" ~ "USGS CAWSC",
-      Source == "USGS_SFBS" ~ "USGS SFBS",
-      TRUE ~ Source
-    ),
-    Parameter = factor(Parameter, levels = param_lev)
-  ) %>%
   summarize(
     min_yr = min(YearAdj),
     max_yr = max(YearAdj),
     .by = c(Source, Parameter)
   ) %>%
-  arrange(Source, Parameter)
+  # Join crosswalk tables for parameter and survey names
+  left_join(df_param_cw, by = join_by(Parameter)) %>%
+  left_join(df_survey_cw, by = join_by(Source)) %>%
+  select(-c(Source, Parameter)) %>%
+  arrange(min_yr, Parameter_publ, .locale = "en") %>%
+  # Create period of record column combining min and max years
+  unite("Period_of_record", min_yr, max_yr, sep = " - ") %>%
+  # Combine parameters into one column for each unique combination of records
+  chop(Parameter_publ) %>%
+  mutate(Parameters_collected = map_chr(Parameter_publ, ~ str_c(.x, collapse = ", "))) %>%
+  # Clean up
+  transmute(
+    Survey = Survey_name,
+    Abbreviation = factor(Source_publ, levels = df_survey_cw$Source_publ),
+    Operator,
+    Parameters_collected,
+    Period_of_record
+  ) %>%
+  arrange(Abbreviation)
 
 # Export survey summary table to be used in the publication.
 df_survey_summ %>% write_csv(here("results/tables/survey_summary_table.csv"))
--- src/data_processing/global_data_proc_func.R
+++ src/data_processing/global_data_proc_func.R
@@ -1,6 +1,6 @@
 # WQ-LT Drought Publication
-# Purpose: Global functions to be used across data processing for the WQ-LT
-  # drought publication
+# Purpose: Global functions and crosswalk tables to be used across data
+  # processing for the WQ-LT drought publication
 # Author: Dave Bosworth
 # Contact: David.Bosworth@water.ca.gov
 
@@ -15,10 +15,39 @@ calc_wy_day <- function(date_var) {
   leap_yr <- lubridate::leap_year(date_var)
   doy <- lubridate::yday(date_var)
   dplyr::case_when(
-    leap_yr == TRUE & doy >= 275 ~ doy -274,
+    leap_yr == TRUE & doy >= 275 ~ doy - 274,
     leap_yr == TRUE & doy < 275 ~ doy + 92,
     leap_yr == FALSE & doy >= 274 ~ doy - 273,
     leap_yr == FALSE & doy < 274 ~ doy + 92
   )
 }
 
+# Crosswalk table for converting parameter names to those used in the
+  # publication
+df_param_cw <- tibble::tribble(
+  ~ Parameter, ~ Parameter_publ,
+  "Temperature", "water temperature",
+  "Salinity", "salinity",
+  "Secchi", "Secchi depth",
+  "DissAmmonia", "ammonium",
+  "DissNitrateNitrite", "nitrate + nitrite",
+  "DissOrthophos", "ortho-phosphate",
+  "Chlorophyll", "chlorophyll"
+)
+
+# Crosswalk table for survey names used in the publication
+df_survey_cw <- tibble::tribble(
+  ~ Source, ~ Source_publ, ~ Survey_name, ~ Operator,
+  "20mm", "20 mm", "20 mm Survey", "CDFW",
+  "Baystudy", "Bay Study", "Bay Study", "CDFW",
+  "FMWT", "FMWT", "Fall Midwater Trawl", "CDFW",
+  "SKT", "SKT", "Spring Kodiak Trawl", "CDFW",
+  "STN", "STN", "Summer Townet Survey", "CDFW",
+  "DJFMP", "DJFMP", "Delta Juvenile Fish Monitoring Program", "USFWS",
+  "EMP", "EMP", "Environmental Monitoring Program", "CDWR",
+  "NCRO", "NCRO", "North Central Region Office monitoring program", "CDWR",
+  "SDO", "SDO", "Stockton Dissolved Oxygen monitoring", "CDWR",
+  "Suisun", "Suisun", "Suisun Marsh Fish Study", "UCD",
+  "USGS_CAWSC", "USGS CAWSC", "Sacramento River at Freeport, California (11447650)", "USGS",
+  "USGS_SFBS", "USGS SFBS", "San Francisco Bay Water Quality Program", "USGS"
+)
